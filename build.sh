b60174ad9218f8f4e9be21beca9cfbcc16eb0b1d
Tsuyoshi Kawasaki <sambatriste@gmail.com>
2016-02-07 00:43:16 +0900

    hoge

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ORG_FILES=*/*.org
+for org_file in $ORG_FILES
+do
+    echo "Processing ${org_file} file..."
+    emacs --batch -q --no-site-file -l org --visit ${org_file} --funcall org-html-export-to-html
+done
+
+echo "done."
